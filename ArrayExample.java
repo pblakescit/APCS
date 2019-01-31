ccd32f9693f43ef6973630564018d5310b7afaa9
pblakescit <46540019+pblakescit@users.noreply.github.com>
2019-01-31 10:10:40 -0500

    Create ArrayExample.java

---
+++ ArrayExample.java
@@ -0,0 +1,45 @@
+
+public class ArrayExample
+{
+  //an object of type ArrayExample has 1 field, it is a 2D array of Strings
+  //this array is of 3 people with their corresponding emails. Each element represents 1 person.
+  private String[][] array;
+  
+  //This no args constructor sets the field array to the following values
+  ArrayExample()
+  {
+    array = new String[][]{{"Dan", "dantheman@gmail.com"},
+                           {"Kate","katethealien@hotmail.com"},
+                           {"Sven","Sr8upCoder@netscape.net"}
+                          };
+  }
+  
+  
+  
+  //How to visit each element in a 2D Array
+  //With for loops
+  
+  
+  //With for each loops
+  
+  
+  
+  
+  
+  
+  public static void main(String[] args)
+  {
+    //Print the array
+    
+    //Get mailing list
+    
+    //Method which returns an array
+    
+    //Method which adds an entry
+    
+    //method which deletes an entry.
+    
+    
+  }
+
+}
